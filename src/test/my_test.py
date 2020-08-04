fa3917908e4aca49ed660a320ce99caf492dd238
BB <seven.albany.bi@gmail.com>
2020-08-04 15:43:29 +0800

    create test case for function num_to_word

--- src/test/my_test.py
+++ src/test/my_test.py
@@ -2,7 +2,7 @@ import sys
 import unittest
 import json
 sys.path.append('..')
-from app.talking import number_word_dict
+from app.talking import number_word_dict, num_to_word
 
 
 class TestTalkingClock(unittest.TestCase):
@@ -11,6 +11,17 @@ class TestTalkingClock(unittest.TestCase):
         with open('test_cases.json') as json_file:
             data = json.load(json_file)
             self.assertEqual(number_word_dict(), data['dict_data'])
+
+    def test_num_to_word(self):
+        with open('test_cases.json') as json_file:
+            data = json.load(json_file)
+            # test different numeric time and format
+            for k, v in data['time_data_test'].items():
+                self.assertEqual(num_to_word(k), v)
+            # test wrong parameters' type
+            for k, v in data['time_data_exp_test'].items():
+                self.assertEqual(num_to_word(k), v)
+            self.assertEqual(num_to_word(6088), '-1')
 
 if __name__ == '__main__':
     unittest.main()
